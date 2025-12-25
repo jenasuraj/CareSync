8f5ed7c4e100765c069e7e18a8028f962a0f39c1
jenasuraj <jenasuraj742@gmail.com>
2025-12-25 20:04:00 +0530

    fix: Absence of react.suspense was making issues

--- app/dashboard/patient/appointment/page.tsx
+++ app/dashboard/patient/appointment/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Appointment from '@/ui/Appointment'
+import { Suspense } from 'react'
 
 
 const page = () => {
   return (
+<Suspense fallback={<div>Loading...</div>}>
 <section className='min-h-screen w-full text-gray-700 flex p-2 flex-col items-center justify-center gap-5'>
   <Appointment/>
 </section>
+</Suspense>
   )
 }
 
