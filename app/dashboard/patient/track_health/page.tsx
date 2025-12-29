3f895accaf4d445f23cf91604bf1343616e55878
jenasuraj <jenasuraj742@gmail.com>
2025-12-29 11:42:00 +0530

    Added a fallback text to wait

--- app/dashboard/patient/track_health/page.tsx
+++ app/dashboard/patient/track_health/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
 <section className='min-h-screen w-full text-gray-700 flex p-2 flex-col items-center justify-center gap-5'>
-hello i am in track health
+  <p className='text-cyan-600 text-4xl'>Working on it... <span className='text-green-600 font-extrabold text-5xl'>200</span>  </p>
 </section>
   )
 }
